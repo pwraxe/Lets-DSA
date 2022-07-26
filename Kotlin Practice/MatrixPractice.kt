8ee85c331f105d9fa81b7fd23be3d4fe1415cd6b
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-07-26 06:08:00 +0530

    Update MatrixPractice.kt

--- Kotlin Practice/MatrixPractice.kt
+++ Kotlin Practice/MatrixPractice.kt
@@ -1,7 +1,6 @@
 import java.util.*
 
-//Add Matrix
-
+//Add Matrix  by traditional Method
 fun main() {
 
     //Define Rows and column
@@ -76,3 +75,76 @@ fun main() {
 }
 
 // O(n*n) + O(n*n) + O(n*n) + O(n*n) + O(n*n) + O(n*n) + 6 = O(6*(n*n))  (Bigo of 6 into n square) ...
+
+
+-------------------------------------------------------------------------------
+ 
+//Add Matrix, show it consized method
+
+fun main() {
+
+    //Define Rows and column
+    val rows = 3
+    val column = 3
+
+    //Declare Array
+    val matrix1 = Array(rows) { IntArray(column) }
+    val matrix2 = Array(rows) { IntArray(column) }
+    val matrix3 = Array(rows) { IntArray(column) }
+
+    //Define input
+    val scanner = Scanner(System.`in`)
+    //-----------------------------------------------------> 6 - time
+
+    //Take First Matrix Elements
+    for (i in matrix1.indices) {
+        for (j in matrix1.indices) {
+            print("Enter matrix1[$i][$j] : ")
+            matrix1[i][j] = scanner.nextInt()
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+    println("\n\n")
+
+    //Take Second Matrix Elements
+    for (i in matrix2.indices) {
+        for (j in matrix2.indices) {
+            print("Enter matrix2[$i][$j] : ")
+            matrix2[i][j] = scanner.nextInt()
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+    println("\n\n")
+    //Print First Matrix
+    for (i in matrix1.indices) {
+        println(matrix1[i].contentToString())
+    }
+    //--------------------------------------------------> O(n*n)
+    println("\n\n")
+
+    //Print Second Matrix
+    for (i in matrix2.indices) {
+        println(matrix2[i].contentToString())
+    }
+    //--------------------------------------------------> O(n*n)
+
+
+    //Add Both Matrix1 + Matrix2
+    for (i in matrix1.indices) {
+        for (j in matrix2.indices) {
+            matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+
+    println("\n\n")
+    //Print Sum of Both Matrix
+    for (i in matrix3.indices) {
+        println(matrix3[i].contentToString())
+    }
+    //--------------------------------------------------> O(n*n)
+
+}
+
+    
+
