69f4387b094dd94618d46af850365fd78199b1e5
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-07-26 06:17:49 +0530

    Update MatrixPractice.kt

--- Kotlin Practice/MatrixPractice.kt
+++ Kotlin Practice/MatrixPractice.kt
@@ -213,9 +213,78 @@ fun main() {
         println(matrix3[i].contentToString())
     }
     //--------------------------------------------------> O(n)
-    //--------------> O(3n*n) + O(3n) ==> 3(n+1)  
+    //--------------> O(3n*n) + O(3n) ==> 3(n+1)    ...
 }
 
+
+====================================================================================
+    import java.util.*
+
+//Add Matrix
+
+fun main() {
+
+    //Define Rows and column
+    val rows = 3
+    val column = 3
+
+    //Declare Array
+    val matrix1 = Array(rows) { IntArray(column) }
+    val matrix2 = Array(rows) { IntArray(column) }
+    val matrix3 = Array(rows) { IntArray(column) }
+
+    //Define input
+    val scanner = Scanner(System.`in`)
+    //-----------------------------------------------------> 6 - time
+
+    //Take First Matrix Elements
+    for (i in matrix1.indices) {
+        for (j in matrix1.indices) {
+            print("Enter matrix1[$i][$j] : ")
+            matrix1[i][j] = scanner.nextInt()
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+    println("\n\n")
+
+    //Take Second Matrix Elements
+    for (i in matrix2.indices) {
+        for (j in matrix2.indices) {
+            print("Enter matrix2[$i][$j] : ")
+            matrix2[i][j] = scanner.nextInt()
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+    println("\n\n")
+    //Print First Matrix
+    for (i in matrix1.indices) {
+        println(matrix1[i].contentToString())
+    }
+    //--------------------------------------------------> O(n)
+    println("\n\n")
+
+    //Print Second Matrix
+    for (i in matrix2.indices) {
+        println(matrix2[i].contentToString())
+    }
+    //--------------------------------------------------> O(n)
+
+
+    var isIdentical = true
+    //Add Both Matrix1 + Matrix2
+    outer@for (i in matrix1.indices) {
+        for (j in matrix2.indices) {
+             if(matrix1[i][j] != matrix2[i][j]) {
+                 isIdentical = false
+                break@outer
+             }
+        }
+    }
+    //--------------------------------------------------> O(n*n)
+    println(if(isIdentical) "Yes Identical" else "No, Not Identical")
+    
+    // O(3n*n) + O(2n) => 3n*n + 2n => 3(n+2) ...
+}
 
     
 
