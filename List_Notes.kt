f6b06354ff6484d0a68b7722e9c9d857eed00481
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-08-14 19:01:04 +0530

    List Vs ArrayList

--- List_Notes.kt
+++ List_Notes.kt
@@ -1,10 +1,22 @@
 fun main() {
+
+    //----------------------------------------------------------------------------
     //1. this is immutable in nature
-    //2. This is Read only list,
+    //2. This is Read only list, add element, remove element, by + or -
+    //3. DisAdvantages : For add, Remove each time new list need to create
     val days = listOf("Sun","Mon","Tues")
-    println("Days : $days") //Days : [Sun, Mon, Tues]
+    println("Days : $days") //[Sun, Mon, Tues]
+
+    // We  have to create new one Immutable list
+    val newDays = days + "Wed"
+    println("New Days : $newDays") //[Sun, Mon, Tues, Wed]
+
+    //We Have to create new Immutable list
+    val resetDays = newDays - "Sun"
+    println("reset Days : $resetDays") //[Mon, Tues, Wed]
 
 
+    //----------------------------------------------------------------------------
     //1. This is mutable in nature
     //2. We can add remove, Update values in List
     val months = arrayListOf("Jan","Feb","Mar","April")
