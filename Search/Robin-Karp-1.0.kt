f4a27f699038631670c9d7a34ed563462f6f7218
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-11-13 15:24:44 +0530

    Rabin-Karp (From Book and Own Practice code)

--- Search/Robin-Karp-1.0.kt
+++ Search/Robin-Karp-1.0.kt
@@ -1,3 +1,8 @@
+/**
+** Studied and Practice From Book
+** See Own Practice Code Below this code 
+**/
+
 fun main() {
 
     val string = "ABCDEFGHIJKOTLINLMOONOPQRST"
@@ -72,3 +77,62 @@ fun rollHash(
 
     return (removedFirst + newCharHash).toLong()
 }
+
+//=========================================================================================================================
+
+
+//Rabin-Krp Trying to Solved Own Way with same stategy
+import kotlin.math.pow
+
+fun main() {
+    val text = "ABCDEFGHIJKLMOONOPQRSTUVWXYZ"
+    val pattern = "MOON"
+    println("Index of $pattern : ${text.searchPattern<String>(pattern)}")
+}
+
+fun <T:Comparable<T>> String.searchPattern(pattern: String): Int {
+
+    val patternLength = pattern.length
+    val textLength = this.length - patternLength
+    var subString = this.substring(0,patternLength)
+    val hashCodeOfPattern = hashCodeOfSubString(pattern,patternLength)
+
+    if(hashCodeOfSubString(subString,patternLength) == hashCodeOfPattern && pattern == subString) {
+        return 0
+    }
+
+    for (index in 1 .. textLength) {
+
+        subString = this.substring(index,index+patternLength)
+        val oldSubString = this.substring(index-1,index+patternLength-1)
+        val subTextHash = rollingHash(oldSubString,subString,patternLength)
+        if(subTextHash == hashCodeOfPattern && subString == pattern) {
+            return index
+        }
+    }
+    return -1
+}
+
+fun rollingHash(oldSubString: String, newSubString: String,patternLength: Int) : Long {
+
+    val oldStringHash = hashCodeOfSubString(oldSubString,patternLength)
+    val oldCharHash = hashOfChar(oldSubString[0],patternLength)
+    val newCharHash = hashOfChar(newSubString[newSubString.length-1],patternLength)
+    val hashAfterRemoveOld =  oldStringHash - oldCharHash
+    val hashAfterAddNewChar = hashAfterRemoveOld + newCharHash
+    return hashAfterAddNewChar
+}
+
+fun hashCodeOfSubString(subString: String,patternLength: Int) : Long {
+    var hashCode = 0L
+    subString.forEachIndexed { index, c ->
+        hashCode += hashOfChar(c, patternLength = patternLength)
+    }
+    return hashCode
+}
+
+fun hashOfChar(char: Char, patternLength: Int): Long {
+    return (char.code.toDouble() * (97.0.pow(patternLength))).toLong()
+}
+ 
+
