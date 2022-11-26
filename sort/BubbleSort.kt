4c89279a6f41f795b2a4b01f491d952e429edf10
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-11-26 14:03:52 +0530

    Bubble Sort O(n*n)

---
+++ sort/BubbleSort.kt
@@ -0,0 +1,41 @@
+fun main() {
+    val list = intArrayOf(5,7,2,1,0,2,3,9,55,7,8,25,10,-10)
+    
+    println(list.letsSortByBubble().contentToString())
+
+    println(list.toMutableList().bubbleSort())
+
+}
+
+fun <T : Comparable<T>> MutableList<T>.bubbleSort(): MutableList<T> {
+    for (i in 0 until size) {
+        for (j in 0 until size-1) {
+
+            //if(this[j] > this[i]) {} --> toSort Ascending Order
+            //if(this[j] < this[i]) {} --> toSort Descending Order
+          
+            if(this[i] > this[j]) {
+                var temp = this[j]
+                this[j] = this[i]
+                this[i] = temp
+            }
+        }
+    }
+    return this
+}
+
+fun IntArray.letsSortByBubble() : IntArray {
+    for (i in 0 until this.size) {
+        for (j in 0 until this.size-1) {
+            //if(this[j] > this[i]) {} --> toSort Ascending Order
+            //if(this[j] < this[i]) {} --> toSort Descending Order
+          
+            if(this[j] > this[i]) {
+                val temp = this[i]
+                this[i] = this[j]
+                this[j] = temp
+            }
+        }
+    }
+    return this
+}
