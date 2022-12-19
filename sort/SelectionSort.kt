967ff2707493ba0d6267800e56b95211803dd0b3
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2022-12-19 09:56:36 +0530

    Selection Sort O(n*n)

---
+++ sort/SelectionSort.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val list = intArrayOf(5,3,2,1,7,8,5,2,4,0,1,2)
+    println(selectionSort(list).contentToString())
+}
+
+fun selectionSort(list: IntArray) : IntArray {
+
+    //taking 0 as minimum index, becoz when swap, we will swap using index
+    var minIndex = 0
+
+    //interate 'i' to end of list with 1 index before
+    for (i in 0 until list.size-1) {
+
+        //j start from 1st index, to find minimum than 'minIndex' value index
+        for (j in (i+1) until list.size) {
+            if(list[minIndex] > list[j]) minIndex = j
+        }
+        //here we will have minValue index, we will swap element using index
+        val temp = list[minIndex]
+        list[minIndex] = list[i]
+        list[i] = temp
+    }
+    return list
+}
