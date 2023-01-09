9663ef7b2cdd3852a3c53d7f2531f3df69a173d2
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-01-09 11:37:02 +0530

    HeapSort O(n log n)

--- sort/HeapSort.kt
+++ sort/HeapSort.kt
@@ -1,3 +1,50 @@
+//-----------------------Own Understanding-------------------
+/**
+ * Complexity : O(n log n)
+ * **/
+fun main() {
+    val list =  intArrayOf(8,5,7,3,1,0,4,6,9,2,78,62)
+    val sorted = heapSort(list)
+    println("UnSorted List : ${list.toTypedArray().contentToString()}")
+    println("Sorted List : ${sorted.contentToString()}")
+}
+
+fun heapSort(list: IntArray): IntArray {t
+
+    val middleIndex = list.size / 2 - 1
+    //Sort in Descending Order
+    for (i in middleIndex downTo 0) {
+        heapify(list,list.size,i)
+    }
+    println("List --> ${list.contentToString()}")
+    //Swap First and last to sort ascending
+    for (i in list.size-1 downTo 0) {
+        //Swap i and 0th element
+        list[0] = list[i].also { list[i] = list[0] }
+        heapify(list,i,0)
+    }
+    return list
+}
+
+fun heapify(list: IntArray, heapSize: Int,rootIndex: Int) {
+    var largeElementIndex = rootIndex
+    val leftNodeIndex = 2 * rootIndex + 1
+    val rightNodeIndex = 2 * rootIndex + 2
+
+    if(leftNodeIndex < heapSize && list[leftNodeIndex] > list[largeElementIndex]) largeElementIndex = leftNodeIndex
+    if(rightNodeIndex < heapSize && list[rightNodeIndex] > list[largeElementIndex]) largeElementIndex = rightNodeIndex
+
+    //We are not swap when root Element is Larger than both its child, else swap
+    if(largeElementIndex != rootIndex) {
+        list[rootIndex] = list[largeElementIndex].also { list[largeElementIndex] = list[rootIndex] }
+        heapify(list,heapSize,largeElementIndex)
+    }
+}
+
+
+
+//-----------------------From Book---------------------------
+
 fun main() {
     val list =  intArrayOf(8,5,7,3,1,0,4,6,9,2)
     val sorted = list.toTypedArray().heapSort()
