9255299ed14ee4db88ec6e8fbe233112afad0c31
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-03-30 11:24:15 +0530

    Depth-first traversal

---
+++ DSA_Kodec_Book/Tree/DepthFirstTravarsal.kt
@@ -0,0 +1,58 @@
+typealias Visitors<T> = (TreeNode<T>) -> Unit
+
+class TreeNode<T>(var dataItem: T) {
+
+    private var children = mutableListOf<TreeNode<T>>()
+    fun addNode(node: TreeNode<T>) {
+        children.add(node)
+    }
+    //Starts Travel from Root Node : Root -> Left -> Right
+    fun forEachDepthFirst(visitors: Visitors<T>) {
+        visitors(this)
+        children.forEach {
+            it.forEachDepthFirst(visitors)
+        }
+    }
+}
+fun addNodes() : TreeNode<Int> {
+    val one = TreeNode(1)
+    val two = TreeNode(2)
+    val four = TreeNode(4)
+    val eight = TreeNode(8)
+    val sixteen = TreeNode(16)
+    val thirty2 = TreeNode(32)
+    val sixty4 = TreeNode(64)
+    val one28 = TreeNode(128)
+    val two56 = TreeNode(256)
+    val five12 = TreeNode(512)
+    val ten24 = TreeNode(1024)
+    val twoK48 = TreeNode(2048)
+    val fourK96 = TreeNode(4096)
+
+    //Lets add
+    one.addNode(two)
+    one.addNode(four)
+
+    two.addNode(eight)
+    two.addNode(sixteen)
+
+    four.addNode(thirty2)
+    four.addNode(sixty4)
+
+    eight.addNode(one28)
+
+    sixteen.addNode(two56)
+    sixteen.addNode(five12)
+
+    sixty4.addNode(ten24)
+    sixty4.addNode(twoK48)
+    sixty4.addNode(fourK96)
+    
+    return one
+}
+
+fun main() {
+    addNodes().forEachDepthFirst {
+        println(it.dataItem)
+    }
+}
