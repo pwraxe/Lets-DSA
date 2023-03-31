412604ebf5a95d575a4bcb14d1df46417c3c740c
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-03-31 10:44:12 +0530

    Depth First Travel | 3 Codes

--- DSA_Kodec_Book/Tree/DepthFirstTravarsal.kt
+++ DSA_Kodec_Book/Tree/DepthFirstTravarsal.kt
@@ -92,3 +92,53 @@ fun main() {
         println(it.dataItem)
     }
 }
+=====================================================================================================================================
+    
+    typealias Visitor<T> = (TreeNode<T>) -> Unit
+
+class TreeNode<T>(val dataItem: T) {
+    private var children = mutableListOf<TreeNode<T>>()
+
+    fun addNode(node: TreeNode<T>) {
+        children.add(node)
+    }
+
+    fun letsTravelToDepthFirst(visitor: Visitor<T>) {
+        visitor(this)
+        children.forEach {
+            it.letsTravelToDepthFirst(visitor)
+        }
+    }
+}
+
+fun main() {
+
+    val root = TreeNode(1)
+    val node1 = TreeNode(10)
+    val node11 = TreeNode(5)
+    val node12 = TreeNode(7)
+
+    val node2 = TreeNode(20)
+    val node21 = TreeNode(2)
+    val node211 = TreeNode(8)
+    val node212 = TreeNode(11)
+
+    val node22 = TreeNode(9)
+
+    //To add or Connect them
+    root.addNode(node1)
+    root.addNode(node2)
+
+    node1.addNode(node11)
+    node1.addNode(node12)
+
+    node2.addNode(node21)
+    node2.addNode(node22)
+
+    node21.addNode(node211)
+    node21.addNode(node212)
+
+    root.letsTravelToDepthFirst {
+        println(it.dataItem)
+    }
+}
