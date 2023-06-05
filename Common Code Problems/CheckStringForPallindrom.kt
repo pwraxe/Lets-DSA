d4653d9903cc3903f1c7490e7e9e0d987db07d9d
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-06-05 21:05:14 +0530

    Check String For Pallindrom

--- Common Code Problems/CheckStringForPallindrom.kt
+++ Common Code Problems/CheckStringForPallindrom.kt
@@ -2,6 +2,7 @@
 
 fun main() {
     println("akshay".isPallindrom())
+    println(checkForPallindrom("Hello")) //false
 }
 
 fun String.isPallindrom() :Boolean {
@@ -9,6 +10,18 @@ fun String.isPallindrom() :Boolean {
     for(i in 0 .. this.length/2) {
         if(this[i] != this[this.length-i-1]) return false
     }
-    return true
+    return true   
+}
+
+fun checkForPallindrom(text: String): Boolean {
     
+    var left = 0
+    var right = text.length-1
+    while(left < right) {
+        if(text[left] != text[right]) return false
+        left++
+        right--
+    }
+    
+    return true
 }
