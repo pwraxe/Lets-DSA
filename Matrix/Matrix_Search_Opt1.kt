fecd12f87269ca1b3adc4eac4d9080349c036ac2
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-06-30 22:51:27 +0530

    Matrix Search Optimal Way 1 and 2

--- Matrix/Matrix_Search_Opt1.kt
+++ Matrix/Matrix_Search_Opt1.kt
@@ -1,3 +1,5 @@
+//Matrix Search Optimal Way 1
+
 import kotlin.random.Random
 fun main() {
 
@@ -31,3 +33,48 @@ fun main() {
         if(twoD[startIndex][endIndex] > key) endIndex-- else startIndex++
     }
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Matrix Search Optimal Way 2
+
+fun main() {
+
+    val row = 4
+    val column = 4
+    val twoD = arrayOf(
+        intArrayOf(10, 20, 30, 40), //list1,
+        intArrayOf(15, 25, 35, 45), //list2,
+        intArrayOf(27, 29, 37, 48), //list3,
+        intArrayOf(32, 33, 39, 50), //list4
+    )
+
+    for (r in 0 until row) {
+        for (c in 0 until column) {
+            print("${twoD[r][c]}\t")
+        }
+        println()
+    }
+
+    val key = 16
+    val isFound = searchUsingBinary(twoD, key)
+    println("Is $key Found? : $isFound ")
+}
+
+fun searchUsingBinary(matrix: Array<IntArray>, key : Int) : Boolean {
+    if (matrix.isEmpty()) return false
+
+    val rowSize = matrix.size
+    val columnSize = matrix[0].size
+
+    var low = 0
+    var high =  (rowSize * columnSize) - 1
+
+    while (low <= high) {
+        val midIndex = (low + high) / 2
+        if(matrix[midIndex/columnSize][midIndex%columnSize] == key) return true
+        if(matrix[midIndex/columnSize][midIndex%columnSize] > key) low = midIndex+1 else high = midIndex-1
+    }
+    return false
+}
+
