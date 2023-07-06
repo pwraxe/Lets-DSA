38d4106f97912b86a0af9d1cdff6e68c9045c350
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-07-06 18:48:43 +0530

    Duplicate Of Zeros, Optimal Included

--- Leetcode Practice/Array/Array_DuplicateOfZeros.kt
+++ Leetcode Practice/Array/Array_DuplicateOfZeros.kt
@@ -14,3 +14,24 @@ class Solution {
         }
     }
 }
+
+//============================================== Other Optimal
+class Solution {
+    fun duplicateZeros(arr: IntArray): Unit {
+       var i = 0
+        while (i < arr.size) {
+            if (arr[i] == 0) {
+                var j = arr.size - 1
+                while (j > i) {
+                    arr[j] = arr[j - 1]
+                    j--
+                }
+                if (i < arr.size - 1) {
+                    arr[i + 1] = 0
+                    i++
+                }
+            }
+            i++
+        }
+    }
+}
