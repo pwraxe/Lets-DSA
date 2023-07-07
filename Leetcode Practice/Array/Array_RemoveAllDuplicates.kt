f64713b4c7c00eb9387f2614114e32e827e093fe
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-07-07 19:19:16 +0530

    Remove All Duplicates Items / O(n)

---
+++ Leetcode Practice/Array/Array_RemoveAllDuplicates.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        
+        var k = 0
+        for(i in 0 until nums.size) {
+            if(nums[k] != nums[i]) {
+                k++
+                nums[k] = nums[i]
+            }
+        }
+        return k+1
+    }
+}
