8d81ea563ac7029489da3b3682fc9c7419f89b0f
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-07-20 20:43:56 +0530

    Add Binary String

---
+++ Leetcode Practice/String/AddStringBinary.kt
@@ -0,0 +1,26 @@
+//Leetcode: Add Binary
+//TC: O(n), max length of input string 
+//SC: O(n), new String created , (but it can be O(1) as we have single string as output)
+
+
+class Solution {
+    fun addBinary(a: String, b: String): String {
+        var result = StringBuilder()
+        var lastIndexOfA = a.length-1
+        var lastIndexOfB = b.length-1
+        var sum = 0
+        var carry = 0
+        
+        while(lastIndexOfA >= 0 || lastIndexOfB >= 0) {
+            sum = carry 
+            if(lastIndexOfA >= 0) sum += a[lastIndexOfA] - '0'
+            if(lastIndexOfB >= 0) sum += b[lastIndexOfB] - '0'
+            result.append("${sum%2}")
+            carry = sum / 2 
+            lastIndexOfA--
+            lastIndexOfB--
+        }
+        if(carry == 1) result.append("1")
+        return result.reverse().toString()
+    }
+}
