385895ee344de352243dc9daa777fb55c88ab173
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-08-02 18:46:41 +0530

    NoOfIslands/ValidParanthesis/PostfixExpression/CloneGraph/TargetSum

--- Leetcode Practice/Repeat Practice on Google Sheet /Practice.kt
+++ Leetcode Practice/Repeat Practice on Google Sheet /Practice.kt
@@ -2,13 +2,61 @@
 1. No of islands
 2. Valid Parenthesis
 3. PostFix Expression
-15Mins
-
-4. Clone Graph: 20mins
-5. Target Sum: 10mins
+4. Clone Graph
+5. Target Sum
 **/
 
+//Time: ~6Min
+//1. No of islands
+class SolutionOfNoOfIslands {
+	private var noOfIsland = 0
+	
+    fun numIslands(grid: Array<CharArray>): Int {
+		val rowSize = grid.size
+		val colSize = grid[0].size
+		for(row in 0 until rowSize) {
+			for(col in 0 until colSize) {
+				if(grid[row][col] == '1') {
+					noOfIsland++
+					doBFS(row, col, grid)
+				}
+			}
+		}
+		return noOfIsland 
+	}
 
+	private fun doBFS(row: Int, col: Int, grid: Array<CharArray>) {
+		val start = row >= 0 && col >= 0
+		val end = row < grid.size && col < grid[0].size
+		if(start && end && grid[row][col] == '1') {
+			doBFS(row, col-1, grid)
+			doBFS(row-1, col, grid)
+			doBFS(row, col+1, grid)
+			doBFS(row+1, col, grid)
+		}
+	}
+}
+//-----------------------------------------------------------------------------------------------
+//Time: ~4Min
+//2. Valid Parenthesis
+class SolutionOfValidParanthesis {
+    fun isValid(str: String) : Boolean {
+        val stack = Stack<Char>()
+        str.toCharArray().forEach {
+            when (it) {
+				'(' -> stack.push(')')
+				'[' -> stack.push(']')
+				'{' -> stack.push('}')
+				else -> {
+					if(stack.isEmpty()) return false	
+					if(stack.pop() != it) return false
+				}	
+			}
+		}
+		return stack.isEmpty()
+	}
+}
+//-----------------------------------------------------------------------------------------------
 //Time: 4mins
 //Reverse Polish Notation
 class SolutionForPostfixExpression {
@@ -39,55 +87,73 @@ class SolutionForPostfixExpression {
 	}
 }
 
-//-----------------------------------------------------------------------------------------------
 
-//Time: ~4Min
-class SolutionOfValidParanthesis {
-    fun isValid(str: String) : Boolean {
-        val stack = Stack<Char>()
-        str.toCharArray().forEach {
-            when (it) {
-				'(' -> stack.push(')')
-				'[' -> stack.push(']')
-				'{' -> stack.push('}')
-				else -> {
-					if(stack.isEmpty()) return false	
-					if(stack.pop() != it) return false
-				}	
-			}
-		}
-		return stack.isEmpty()
+//-----------------------------------------------------------------------------------------------Recursive
+//4. Clone Graph
+//Time: ~7Min
+class SolutionForCloneGraph {
+    private fun doClone(node: Node?, map: HashMap<Node?, Node?>) : Node? {
+        
+        node ?: return null
+        
+        map[node]?.let { return it }
+        
+        val newNode = Node(node?.`val`)
+        map[node] = newNode
+        
+        node?.neighbors.forEach {
+            newNode?.neighbors.add(doClone(it,map))
+        }
+        return newNode
+    }
+
+	fun cloneGraph(node: Node?): Node? {
+		return doClone(node, hashMapOf<Node?, Node?>())
 	}
 }
+//-----------------------------------------------------------------------------------------------Iterative
+//Time: 6mins
+//4. Clone Graph
+class SolutionForCloneGraph {
+	fun cloneGraph(node: Node?): Node? {
+		node ?: return null
+    
+    	val stack = Stack<Node>()
+		val map = hashMapOf<Node?,Node?>()
 
-//-----------------------------------------------------------------------------------------------
+		stack.push(node)
+    	val newNode = Node(node?.`val`)
+    	map[node] = newNode
 
-//Time: ~6Min
-class SolutionOfNoOfIslands {
-	private var noOfIsland = 0
-	
-    fun numIslands(grid: Array<CharArray>): Int {
-		val rowSize = grid.size
-		val colSize = grid[0].size
-		for(row in 0 until rowSize) {
-			for(col in 0 until colSize) {
-				if(grid[row][col] == '1') {
-					noOfIsland++
-					doBFS(row, col, grid)
+    	while(stack.isNotEmpty()) {
+			val top = stack.pop()
+			for(adj in top.neighbors) {	
+				if(map[adj] == null) {
+					map[adj] = Node(adj?.`val`!!)
+					stack.push(adj)
 				}
+				map[top]?.neighbors?.add(map[adj])
 			}
 		}
-		return noOfIsland 
+		return newNode
 	}
+} 
+//-----------------------------------------------------------------------------------------------
 
-	private fun doBFS(row: Int, col: Int, grid: Array<CharArray>) {
-		val start = row >= 0 && col >= 0
-		val end = row < grid.size && col < grid[0].size
-		if(start && end && grid[row][col] == '1') {
-			doBFS(row, col-1, grid)
-			doBFS(row-1, col, grid)
-			doBFS(row, col+1, grid)
-			doBFS(row+1, col, grid)
+//Time: ~4Mins
+class SolutionForFindTarget {
+	private var targetSum = 0
+
+	private fun findTarget(nums: IntArray, target: Int, index: Int, sum: Int) {
+		if(index == nums.size) {
+			if(sum == target) return targetSum++
+		} eles {
+			findTarget(nums, target, index+1, sum+nums[index])
+			findTarget(nums, target, index+1, sum-nums[index])
 		}
+	}
+	fun findTargetSumWays(nums: IntArray, target: Int): Int {
+		findTarget(nums, target, 0, targetSum)
+		return targetSum
 	}
 }
