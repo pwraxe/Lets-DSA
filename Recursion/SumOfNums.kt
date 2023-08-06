65f69dd58f251cbe1ec9583394141a03db977dd8
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2023-08-06 19:30:39 +0530

    Sum Of Nums  || Head Recursion

--- Recursion/SumOfNums.kt
+++ Recursion/SumOfNums.kt
@@ -1,10 +1,18 @@
 //Sum of 10 nums //ref. from factorial 
 fun main() {
-	val sum = sumTo(1)
+    val sum = sumTo(1)
     println("Sum of 10: $sum")
 }
 
+//Head Recursion || Bottom-Up Approach
+//Going to the End till condition and then execurtes function body
 fun sumTo(n: Int) : Int {
+    //Base Condition
     if(n == 10) return n
-    return n + sumTo(n+1)
+    
+    //Funciton call || Head Recusrion
+    val res = sum(n+1)
+    
+    //Function Body
+    return n + res
 }
