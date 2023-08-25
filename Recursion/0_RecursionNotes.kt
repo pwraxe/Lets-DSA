d8dce8fc3327c2c98c451935a04eb0d1181c0a84
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2023-08-25 19:34:37 +0530

    0_RecursionNotes.kt

--- Recursion/0_RecursionNotes.kt
+++ Recursion/0_RecursionNotes.kt
@@ -1,4 +1,30 @@
 Permutation Vs TakeNotTake 
+--------------------------
+  
+Permutation
+- In the Permutation Method, We swap elements twice in a loop before and after the recursive function call
+Ex. 
+fun todo() {
 
-In Permutation Method We swap element twice in loop
-In Take not take Method, we add element in sublist, and then remobve element from sublist and recursive call twice, after adding and after removing element from subList
+  //Base Condition
+  loop(index to input.size) {
+    
+    swap() //swapping for the next combination in the same depth
+    todo()
+    swap() //undo swapping for the next combination in the next tree branch
+  }
+}
+===========================================================================================
+  
+TakeNotTake || Backtracking
+  - In the take-not-take Method, we add an element in the sublist, then remove an element from the sublist and recursive call twice, 
+  - In this, we call the function recursively only once, before that we add an element, and after the function, we remove an element from 
+
+Ex. 
+fun todo() {
+  //base condition
+  add Element
+  todo(...)
+  remove element
+}
+
