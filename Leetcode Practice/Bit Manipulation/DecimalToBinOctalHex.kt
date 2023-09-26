7388fef3b94bc00d98b56c6ce681d9a9e1ea2d69
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2023-09-26 20:27:59 +0530

    Update DecimalToBinOctalHex.kt

--- Leetcode Practice/Bit Manipulation/DecimalToBinOctalHex.kt
+++ Leetcode Practice/Bit Manipulation/DecimalToBinOctalHex.kt
@@ -3,6 +3,12 @@ fun main() {
     println(toBinary(decimal, StringBuilder()))
     println(toOctal(decimal, StringBuilder()))
     println(toHex(decimal, StringBuilder()))
+
+    //Fraction decimal to binary
+    val fraction = 1857.625
+    val binaryOfBeforeDecimal = toBinary(fraction.toInt(), StringBuilder())
+    val binaryOfAfterDecimal = toFractionBinary(fraction-fraction.toInt(), StringBuilder())
+    println("${binaryOfBeforeDecimal}.${binaryOfAfterDecimal}")
 }
 
 //Note: For  Convert to Binary, We Devide and Mod by 2
@@ -10,6 +16,17 @@ fun toBinary(num:Int, binary:StringBuilder): Long{
     if (num == 0) return binary.toString().reversed().toLong()
     return toBinary(num/2,binary.append(num % 2))
 }
+
+//0.625 x 2 = 1.25 with Integer 1
+//0.25 x 2 = 0.5 with Integer 0
+//0.5 x 2 = 1.0 with Integer 1
+//0.0 x 2 = 0.0 with Integer 0
+fun toFractionBinary(num: Double, binary: StringBuilder): Long {
+    if(num == 0.0) return binary.toString().reversed().toLong()
+    val n = num * 2.0
+    return toFractionBinary(n-n.toInt(), binary.append(n.toInt()))
+}
+
 
 //Note: For  Convert to Octal, We Devide and Mod by 8
 fun toOctal(num:Int, octal:StringBuilder): Int {
