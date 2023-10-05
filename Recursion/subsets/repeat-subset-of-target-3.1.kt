aa7c4f8e4435e08b0d4e8943e938cc42508a1dcd
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2023-10-05 12:29:23 +0530

    Update repeat-subset-of-target-3.1.kt

--- Recursion/subsets/repeat-subset-of-target-3.1.kt
+++ Recursion/subsets/repeat-subset-of-target-3.1.kt
@@ -1,3 +1,9 @@
+//NOTE: Code is SAME, Just Look at Output,
+//For Each Output, had practice of code
+
+
+
+
 //repeat-subset-of-target-3.1.kt
 fun main() {
     val list = mutableListOf<List<Int>>()
@@ -27,5 +33,14 @@ fun getTargetSubList(nums: IntArray, index :Int, target: Int, sum: Int,subList:M
     subList.removeLast()
 }
 
-//OUTPUT
+// Input: [1,2], Target: 4
+// OUTPUT
 // [[2, 2], [1, 1, 2], [1, 1, 1, 1]]
+//============================================================================================================================================
+
+//Input: [1,1,1], Target: 8
+/** OUTPUT
+[
+    [1, 1, 1, 1, 1, 1, 1, 1], //45 times
+]
+**/
