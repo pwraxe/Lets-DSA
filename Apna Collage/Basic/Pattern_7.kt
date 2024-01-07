2642532a219fca94b7e683aa3759afa2b39b06a1
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-07 20:14:14 +0530

    Update Pattern_7.kt

--- Apna Collage/Basic/Pattern_7.kt
+++ Apna Collage/Basic/Pattern_7.kt
@@ -9,6 +9,31 @@ fun main() {
     }
 }
 
+
+//======================================================
+class Solution {
+    private val rows = 5
+    fun pattern(row:Int, col:Int, sum:Int) {
+        if (row == rows) return
+        if (col <= row) {
+            print("$sum ")
+            pattern(row, col+1, sum+1)
+        } else {
+            println()
+            pattern(row+1, 0, sum)
+        }
+    }
+}
+
+fun main() {
+    Solution().apply {
+        pattern(0,0, 1)
+    }
+}
+
+
+
+
 /***********************
 1 
 2 3 
