77c67f61410f2805b848d221ef6e911b76b92d37
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-07 20:55:31 +0530

    Create Pattern_11.kt

---
+++ Apna Collage/Basic/Pattern_11.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val rows = 5
+    var space = 5
+    for (row in rows downTo 1) {
+        repeat(space--) {
+            print("  ")
+        }
+        if (row == 1 || row == rows) {
+            repeat(rows) {
+                print("* ")
+            }
+        } else {
+            print("* ")
+            repeat(rows-2) {
+                print("  ")
+            }
+            print("* ")
+        }
+
+        println()
+    }
+}
