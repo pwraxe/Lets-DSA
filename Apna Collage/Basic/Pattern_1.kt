b57895fbb61bb20423d5f154d38d7f800c0ac43c
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-07 19:23:47 +0530

    Update Pattern_1.kt

--- Apna Collage/Basic/Pattern_1.kt
+++ Apna Collage/Basic/Pattern_1.kt
@@ -8,6 +8,28 @@ fun main() {
         println()
     }
 }
+
+//=====================================================================
+//Recursive
+class Solution {
+
+    fun pattern(row: Int, col:Int = 0) {
+        if (row == 0) return
+        if (col < row) {
+            print("* ")
+            pattern(row, col+1)
+        } else {
+            println()
+            pattern(row-1, 0)
+        }
+    }
+}
+fun main() {
+    Solution().apply {
+        pattern(7)
+    }
+}
+
 /*
 
 * * * * * * *
