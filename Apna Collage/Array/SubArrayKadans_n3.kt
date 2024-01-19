639c351687307cab94a065eeaad02a22706d4554
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-19 16:30:35 +0530

    Create SubArrayKadans_n3.kt

---
+++ Apna Collage/Array/SubArrayKadans_n3.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun printSubArray(n:IntArray) {
+        var maxSum = 0
+        for (s in 1 .. n.size) {
+
+            for (i in 0 .. n.size-s) {
+                var sum = 0
+                for (j in i until i + s) {
+                    sum += n[j]
+                }
+                maxSum = Math.max(sum, maxSum)
+            }
+        }
+        println("MAX SUM: $maxSum")
+    }
+
+    fun kadensSum(n:IntArray) {
+
+        var sum = 0
+        var maxSum = Int.MIN_VALUE
+        for (i in n.indices) {
+            sum += n[i]
+            maxSum = Math.max(maxSum, sum)
+            if (sum < 0) sum = 0
+        }
+        println("Kaneds Max: $maxSum")
+    }
+}
+fun main() {
+    Solution().apply {
+        val list = intArrayOf(2,-3,5,-6,-7,8,4,-1)
+//        val list = intArrayOf(2,-3,4,-1, -2, 1,5,-3)
+        printSubArray(list)
+        kadensSum(list)
+    }
+}
