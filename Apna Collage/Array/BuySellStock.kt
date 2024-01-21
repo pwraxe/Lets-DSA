ae464bc56350e40d09d3c7350b34cbec4d4356bd
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-21 18:22:17 +0530

    Create BuySellStock.kt

---
+++ Apna Collage/Array/BuySellStock.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun buySellStock(n:IntArray) {
+
+        var buyPrice = n.first()
+        var profit = 0
+
+        for (index in 1..<n.size) {
+            //buyPrice = Math.min(buyPrice, n[index])
+            //profit = Math.max(profit, n[index] - buyPrice)
+          
+            buyPrice = buyPrice.coerceAtMost(n[index])
+            profit = profit.coerceAtLeast(n[index] - buyPrice)
+        }
+        println("Max Profit: $profit")
+    }
+}
+fun main() {
+    Solution().apply {
+        buySellStock(intArrayOf(7,1,5,3,6,4))
+    }
+}
+
