f8ec6d4eef9208d2c3cd6309238e62466c6b6a5e
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-01-25 12:54:57 +0530

    Create StringCompress.kt

---
+++ Apna Collage/String/StringCompress.kt
@@ -0,0 +1,26 @@
+class Solution {
+     fun stringCompress(str: String): String {
+         val freq = IntArray(26)
+         for (i in str.indices) {
+             val index = str[i] - 'a'
+            freq[index]++
+         }
+         val compress = StringBuilder()
+         for (i in freq.indices) {
+             val char = (97 + i).toChar()
+             if (freq[i] > 1) {
+                 compress.append(char).append(freq[i])
+             } else if(freq[i] == 1) {
+                 compress.append(char)
+             }
+         }
+         return compress.toString()
+     }
+}
+
+fun main() {
+    Solution().apply {
+        println(stringCompress("aaaabbcccdd"))
+        println(stringCompress("abcd"))
+    }
+}
