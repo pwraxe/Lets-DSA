10616edf710a26e6f80fab5e95fc36bafd5b3325
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-07-17 08:44:45 +0530

    Bubble | Selection | Insertion | Counting | Merge

--- Apna Collage 2.0/Sort/All_Sorting.kt
+++ Apna Collage 2.0/Sort/All_Sorting.kt
@@ -1,7 +1,16 @@
 //Bubble Sort ASC
 //Bubble Sort DESC
+
 //Selection Sort ASC
 //Selection Sort DESC
+
+//InsertionSort ASC
+//InsertionSort DESC
+
+//Counting Sort ASC
+//Counting Sort DESC
+
+//Merge Sort
 
 class Solution {
 
@@ -56,6 +65,98 @@ class Solution {
 
         println(list.toTypedArray().contentToString())
     }
+
+    fun insertionSortASC(list: IntArray) {
+
+        for (i in 1 ..< list.size) {
+            val temp = list[i]
+            var prev = i-1
+
+            while (prev >= 0 && list[prev] > temp) {
+                list[prev+1] = list[prev]
+                prev--
+            }
+            list[prev+1] = temp
+        }
+
+        println(list.toTypedArray().contentToString())
+    }
+    fun insertionSortDESC(list: IntArray) {
+
+        for (i in 1 ..< list.size) {
+            val temp = list[i]
+            var prev = i-1
+
+            while (prev >= 0 && list[prev] < temp) {
+                list[prev+1] = list[prev]
+                prev--
+            }
+            list[prev+1] = temp
+        }
+        println(list.toTypedArray().contentToString())
+    }
+
+    fun countingSortASC(list: IntArray) {
+        val res = IntArray(list.size)
+        for (num in list) {
+            res[num]++
+        }
+        var index = 0
+        for (i in res.indices) {
+            val count = res[i]
+            repeat(count) {
+                list[index++] = i
+            }
+        }
+        println(list.toTypedArray().contentToString())
+    }
+    fun countingSortDESC(list: IntArray) {
+        val res = IntArray(list.size)
+        for (num in list) {
+            res[num]++
+        }
+
+        var index = 0
+        for (i in res.size-1 downTo 0) {
+            val count = res[i]
+            repeat(count) {
+                list[index++] = i
+            }
+        }
+        println(list.toTypedArray().contentToString())
+    }
+
+
+    private fun merge(start: Int, mid: Int, end:Int, list: IntArray) {
+        var left = start
+        var right = mid+1
+        var index = 0
+        val res = IntArray(start+end+1)
+
+        while (left <= mid && right <= end) {
+            if (list[left] <= list[right]) {
+                res[index++] = list[left++]
+            }else {
+                res[index++] = list[right++]
+            }
+        }
+
+        while (left <= mid) res[index++] = list[left++]
+        while (right < end) res[index++] = list[right++]
+
+    }
+    private fun devide(start: Int, end: Int, list: IntArray) {
+        if (start == end) return
+        val mid = (start+end)/2
+        devide(start,mid,list)
+        devide(mid+1, end, list)
+        merge(start, mid, end, list)
+    }
+    fun mergeSort(list: IntArray) {
+        devide(0, list.size-1, list)
+        println(list.toTypedArray().contentToString())
+    }
+
 }
 
 fun main() {
@@ -68,13 +169,12 @@ fun main() {
         selectionSortASC(list)
         selectionSortDESC(list)
         println("========================================")
-
+        insertionSortASC(list)
+        insertionSortDESC(list)
+        println("========================================")
+        countingSortASC(list)
+        countingSortDESC(list)
+        println("========================================")
+        mergeSort(list)
     }
 }
-
-
-[1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-[9, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1]
-========================================
-[1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-[9, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1]
