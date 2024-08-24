7d52de752e378eb9fb161a2472664f844212096b
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-08-24 10:30:36 +0530

    Update PrimeFactors.kt

--- 2.0 Restart/Maths/PrimeFactors.kt
+++ 2.0 Restart/Maths/PrimeFactors.kt
@@ -1,8 +1,32 @@
 class Solution {
 
 
-    //TC: O(sqrt(n) * log n)
+    //TC: O(sqrt(log n))
+    private fun isPrime(num: Int): Boolean {
+        if (num == 2 || num == 3) return true
+        if (num % 2 == 0 || num % 3 == 0) return false
+        for (i in 5 .. Math.sqrt(num.toDouble()).toInt() step 6) {
+            if (num %i == 0 || num % (i+2) == 0) return false
+        }
+        return true
+    }
+    //TC: (n * sqrt(n) * log(n))
     fun getPrimeFactors1(n: Int) {
+
+        for (i in 2 ..< n) {
+            if (isPrime(i)) {
+                var temp = i
+                while (n % temp == 0) {
+                    print("$i ")
+                    temp *= i
+                }
+            }
+        }
+        println()
+    }
+
+    //TC: O(sqrt(n) * log n)
+    fun getPrimeFactors2(n: Int) {
         var num = n
         if(num <= 1) return
 
@@ -15,7 +39,7 @@ class Solution {
         if(num > 1) println("$num ") else println()
     }
 
-    //O(sqrt(n))
+    //O(n + n + sqrt(n)) = O(sqrt(n))
     fun getPrimeFactors3(n: Int) {
         var num = n
         if (num <= 1) return
@@ -48,9 +72,36 @@ class Solution {
 
 fun main() {
     Solution().apply {
-        getPrimeFactors1(737) // 11 67
-        getPrimeFactors3(400) // 2 2 2 2 5 5 1
-        getPrimeFactors3(1092) // 2 2 2 2 5 5 1
+        val x = 737
+        val y = 400
+        val z = 1092
+
+        getPrimeFactors1(x)
+        getPrimeFactors1(y)
+        getPrimeFactors1(z)
+
+        println("=============")
+        getPrimeFactors2(x)
+        getPrimeFactors2(y)
+        getPrimeFactors2(z)
+
+        println("=============")
+        getPrimeFactors3(x)
+        getPrimeFactors3(y)
+        getPrimeFactors3(z)
     }
 }
 
+
+
+11 67 
+2 2 2 2 5 5 
+2 2 3 7 13 
+=============
+11 67 
+2 2 2 2 5 5 
+2 2 3 7 13 
+=============
+11 67
+2 2 2 2 5 5 
+2 2 3 7 13
