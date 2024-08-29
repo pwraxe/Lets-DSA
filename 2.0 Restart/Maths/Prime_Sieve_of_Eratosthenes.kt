fd656510fd17a67d1b189a9e58bbd41b151d455b
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-08-29 19:52:37 +0530

    Update Prime_Sieve_of_Eratosthenes.kt

--- 2.0 Restart/Maths/Prime_Sieve_of_Eratosthenes.kt
+++ 2.0 Restart/Maths/Prime_Sieve_of_Eratosthenes.kt
@@ -1,3 +1,58 @@
+class Solution {
+    //O(n)
+    fun isPrime1(num:Int): Boolean {
+        if(num == 1) return false
+        for(i in 2 ..< num) {
+            if(num % i == 0) return false
+        }
+        return true
+    }
+    fun isPrime2(num: Int): Boolean {
+        if(num == 1) return false
+        for(i in 2 .. Math.sqrt(num * 1.0).toInt()) {
+            if(num % i == 0) return false
+        }
+        return true
+    }
+    fun isPrime3(num: Int): Boolean {
+        if(num == 1) return false
+        if(num == 2 || num == 3) return true
+        if(num % 2 == 0 || num % 3 == 0) return false
+
+        for(i in 5 .. Math.sqrt(num * 1.0).toInt() step 6) {
+            if(num%i == 0 || num%(i+2) == 0) return false
+        }
+        return true
+    }
+
+    //Sieve of Eratosthenes
+    fun isPrime4(num:Int): Boolean {
+        if(num == 1) return false
+        if(num == 2 || num == 3) return true
+        if(num % 2 == 0 || num % 3 == 0 || num % 5 == 0) return false
+        val primes = BooleanArray(num+1) { true }
+        for(i in 2 .. Math.sqrt(num * 1.0).toInt()) {
+            if(primes[i]) {
+                for(j in i+i .. num step i) {
+                    primes[j] = false
+                }
+            }
+        }
+        return primes[num]
+    }
+}
+
+
+fun main() {
+    Solution().apply {
+        println(isPrime1(157))
+        println(isPrime2(157))
+        println(isPrime3(157))
+        println(isPrime4(158))
+    }
+}
+======================================================================
+
 //Get all Prime Nos by Sieve of Eratosthenes
 class Solution {
 
@@ -66,3 +121,4 @@ fun main() {
         soe3(23)
     }
 }
+ 
