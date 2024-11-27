dbac4eddf8115c1472e93c7aaf0c4d6722e01457
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-11-27 15:38:01 +0530

    Create Algorithms.kt

---
+++ 2.0 Restart/Algorithms.kt
@@ -0,0 +1,23 @@
+Recursive Algorithm 
+(Ex. Factorial of a Number, Fibonacci Series, Tower of Hanoi, DFS for Graph, etc.)
+   - D&C (Ex. Binary Search, Merge Sort, Quick Sort, Strassen’s Matrix Multiplication, etc.)
+   - DP (Ex.  Knapsack Problem, Weighted Job Scheduling, Floyd Warshall Algorithm,  etc.)
+   - Greedy (Ex. Dijkstra Shortest Path Algorithm, Prim’s Algorithm, Kruskal’s Algorithm, Huffman Coding, etc.)
+   - Backtracking (Ex. Hamiltonian Cycle, M-Coloring Problem, N Queen Problem, Rat in Maze Problem, etc.)
+
+Backtracking Algorithm 
+(Ex. Binary Search, Merge Sort, Quick Sort, Strassen’s Matrix Multiplication, etc.)
+
+Searching Algorithm ()
+
+Sorting Algorithm ()
+
+Hashing Algorithm
+
+Divide and Conquer Algorithm
+
+Greedy Algorithm
+
+Dynamic Programming
+
+Randomized Algorithm
